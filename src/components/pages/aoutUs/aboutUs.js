eca1df20469f5a0ddeb9c58301dd882df82b2b42
unknown <sundar.badagala@gmail.com>
2021-01-26 11:45:58 +0530

    'add-aboutUs-card'

--- src/components/pages/aoutUs/aboutUs.js
+++ src/components/pages/aoutUs/aboutUs.js
@@ -1,11 +1,18 @@
 import React from 'react'
-
-function AboutUs() {
-    return (
-        <div>
-            ABOUT US
-        </div>
-    )
+import {items} from './aboutUsData'
+import { CardView } from 'react-card-with-image'
+import 'react-card-with-image/dist/index.css'
+ 
+const App = () => {
+  
+  return (
+    <CardView
+      items={items}
+      activeColor='#000'
+      imageHeight='650px'
+      imageWidth='800px'
+    />
+  )
 }
-
-export default AboutUs
+ 
+export default App
