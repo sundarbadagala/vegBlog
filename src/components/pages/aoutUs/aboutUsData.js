a0489a263ca68bf2b9e5251e31c017c79b12a198
sundar rao <sundar.badagala@gmail.com>
2021-07-22 21:39:59 +0530

    'edit-about-us-page'

--- src/components/pages/aoutUs/aboutUsData.js
+++ src/components/pages/aoutUs/aboutUsData.js
@@ -4,20 +4,20 @@ export const items = [
       header: 'CEO',
       description:
         'Steven Paul Jobs was an American business magnate, industrial designer, investor, and media proprietor.',
-      image: 'https://cdn.vox-cdn.com/thumbor/DVN7eqE1o8HeBOP-jg15YHTsiLY=/0x0:640x427/1200x800/filters:focal(0x0:640x427)/cdn.vox-cdn.com/assets/1496753/stevejobs.jpg'
+      image: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'
     },
     {
       id: 2,
       header: 'Managing Director',
       description:
         'William Henry Gates III is an American business magnate, software developer, and philanthropist.',
-      image: 'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg'
+      image: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'
     },
     {
       id: 3,
       header: 'Manager',
       description:
         'Mark Elliot Zuckerberg is an American media magnate, internet entrepreneur, and philanthropist.',
-      image: 'https://image.cnbcfm.com/api/v1/image/104410446-GettyImages-669889288.jpg?v=1529474846&w=1400&h=950'
+      image: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'
     }
   ]
